d37d9062284b6a20c543a35bcede6aa5a8029a4d
avidCutlet <vonwebsterlabajo@gmail.com>
2020-12-17 15:00:35 +0800

    First commit :D

---
+++ src/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <!-- Font link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Poppins">
+    
+    <!-- Other resources -->
+    <link rel="stylesheet" href="../css/imagebg.css">
+    <link rel="stylesheet" href="../css/fontfamily.css">
+    
+  </head>
+<body>
+
+
+<!-- navigator -->
+<div class="sticky-top">
+
+
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    
+    <div class="container-fluid">
+      
+      <div>
+        <div class="hf">
+          <a class="navbar-brand" href="#">Research DB</a>
+        </div>
+    
+        <div class="mt-n3">
+          <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
+        </div>
+      </div>
+
+      <!-- Insert this below the research data bank link
+      <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
+      -->
+
+      <ul class="navbar-nav ml-auto">
+
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+
+        </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+
+        </li>
+
+      </ul>
+
+  
+
+      
+      
+
+    </div>
+  </nav>
+</div>
+
+<!-- body -->
+
+<div class="container-fluid bg">
+  <div class="display-5" style="margin-top: 13%; margin-left: 8%">
+      <h1>Welcome to RSDB</h1>
+      <small style="font-size:larger">Search the knowledge hidden within</small>
+      
+      <!-- Search form -->
+      <div class="input-group mb-3 mt-4" style="width: 50%;">
+        <div class="input-group-prepend">
+          <button class="btn btn-outline-primary" type="button" style="background-color: transparent;">Search</button>
+        </div>
+        <input type="text" class="form-control" style="background-color: transparent;" placeholder="Search for articles...">
+      </div>
+
+
+      <!-- Most search keywords -->
+
+      <div class="container-fluid">
+        <div class="row">
+
+          <div>
+            <h6>Popular Search:</h6>
+          </div>
+
+         
+          <div class="ml-2">
+            <a href="#">Fiction</a>
+          </div>
+
+          <span class="ml-2 mr-2">|</span>
+
+          <div>
+            <a href="#">Renai Circulation</a>
+          </div>
+
+          <span class="ml-2 mr-2">|</span>
+
+          <div>
+            <a href="#">Chika Dance</a>
+          </div>
+          </ul>
+
+
+        </div>
+      </div>
+  </div>
+</div>
+
+
+</body>
+</html>
