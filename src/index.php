e305dea7101fdb228e71b844fc17f138a75aef4a
avidCutlet <vonwebsterlabajo@gmail.com>
2020-12-17 20:48:48 +0800

    Fix bg && Add jumbotron && Add footer etc...

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BSU-SC Research DB</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -17,117 +17,171 @@
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <!-- Font link -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Poppins">
     
     <!-- Other resources -->
     <link rel="stylesheet" href="css/imagebg.css">
-    <link rel="stylesheet" href="css/fontfamily.css">
+    <link rel="stylesheet" href="css/custom.css">
     
   </head>
 <body>
 
 
 <!-- navigator -->
-<div class="sticky-top">
+<div class="bg" style="padding-bottom: 25%;">
+  <div class="sticky-top">
 
 
-  <nav class="navbar navbar-expand-md navbar-light bg-light">
-    
-    <div class="container-fluid">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
       
-      <div>
-        <div class="hf">
-          <a class="navbar-brand" href="#">Research DB</a>
-        </div>
-    
-        <div class="mt-n3">
-          <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
-        </div>
-      </div>
-
-      <!-- Insert this below the research data bank link
-      <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
-      -->
-
-      <ul class="navbar-nav ml-auto">
-
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home</a>
-
-        </li>
-
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-
-        </li>
-
-      </ul>
-
-  
-
-      
-      
-
-    </div>
-  </nav>
-</div>
-
-<!-- body -->
-
-<div class="container-fluid bg">
-  <div class="display-5" style="margin-top: 13%; margin-left: 8%">
-      <h1>Welcome to RSDB</h1>
-      <small style="font-size:larger">Search the knowledge hidden within</small>
-      
-      <!-- Search form -->
-      <div class="input-group mb-3 mt-4" style="width: 50%;">
-        <div class="input-group-prepend">
-          <button class="btn btn-outline-primary" type="button" style="background-color: transparent;">Search</button>
-        </div>
-        <input type="text" class="form-control" style="background-color: transparent;" placeholder="Search for articles...">
-      </div>
-
-
-      <!-- Most search keywords -->
-
       <div class="container-fluid">
-        <div class="row">
-
-          <div>
-            <h6>Popular Search:</h6>
+        
+        <div>
+          <div class="hf">
+            <a class="navbar-brand" href="#">Research DB</a>
           </div>
-
-         
-          <div class="ml-2">
-            <a href="#">Fiction</a>
+      
+          <div class="mt-n3">
+            <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
           </div>
-
-          <span class="ml-2 mr-2">|</span>
-
-          <div>
-            <a href="#">Renai Circulation</a>
-          </div>
-
-          <span class="ml-2 mr-2">|</span>
-
-          <div>
-            <a href="#">Chika Dance</a>
-          </div>
-          </ul>
-
-
         </div>
+
+        <!-- Insert this below the research data bank link
+        <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
+        -->
+
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+
+          </li>
+
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+
+          </li>
+
+        </ul>
+
+    
+
+        
+        
+
       </div>
+    </nav>
+  </div>
+
+  <!-- body -->
+
+  <div class="container-fluid">
+    <div class="display-5" style="margin-top: 13%; margin-left: 8%">
+        <h1>Welcome to RSDB</h1>
+        <small style="font-size:larger">Search the knowledge hidden within</small>
+        
+        <!-- Search form -->
+        <div class="input-group mb-3 mt-4" style="width: 50%;">
+          <div class="input-group-prepend">
+            <button class="btn btn-outline-primary" type="button" style="background-color: transparent;">Search</button>
+          </div>
+          <input type="text" class="form-control" style="background-color: transparent;" placeholder="Search for articles...">
+        </div>
+
+
+        <!-- Most search keywords -->
+
+        <div class="container-fluid">
+          <div class="row" >
+
+            <div>
+              <h6>Popular Search:</h6>
+            </div>
+
+          
+            <div class="ml-2">
+              <a href="#">Fiction</a>
+            </div>
+
+            <span class="ml-2 mr-2">|</span>
+
+            <div>
+              <a href="#">Renai Circulation</a>
+            </div>
+
+            <span class="ml-2 mr-2">|</span>
+
+            <div>
+              <a href="#">Chika Dance</a>
+            </div>
+            </ul>
+
+
+
+          </div>
+        </div>
+    </div>
   </div>
 </div>
+
+
+<!-- Jumbotron -->
+<div class="jumbotron jumbotron-fluid jt">
+  <div class="container">
+
+    <h1>Lorem lorem ipsum</h1>
+    <p>Lorem ipsum dolor sit amet. Lorem ipsum lorem ipsum</p>
+  </div>
+</div>
+
+
+<!-- Footer -->
+
+<footer class="border-top-2 pb-4">
+
+<div class="container">
+  <div class="row">
+    <div class="col-8 ft">
+      <p style="margin-top: -1%">Copyright © 2020 Research DB. All rights reserved.<br>
+        We use cookies to help provide and enhance our service and tailor content.<br>
+        By continuing you, agree to our <a href="#">Cookies Settings</a>.</p><br>
+
+      <div style="margin-top: -4%;">
+        <a href="#">Copyright</a>
+        <span class="px-3">|</span>
+
+        <a href="#">Terms of Use</a>
+        <span class="px-3">|</span>
+        
+        <a href="#">Privacy Policy</a>
+        
+      </div>
+    </div>
+
+    <div class="col-4" style="margin-top: -1%">
+      <span>Follow us on:</span><br>
+
+      <span class="fa fa-facebook-official sl"></span>
+      <span class="fa fa-instagram sl px-3"></span>
+      <span class="fa fa-twitter-square sl"></span>
+    </div>
+
+  </div>
+</div>
+
+</footer>
+
 
 
 </body>
