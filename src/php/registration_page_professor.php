0a4cf0398ecc306766435fa817a513d6f0b418e4
avidCutlet <vonwebsterlabajo@gmail.com>
2020-12-18 23:11:19 +0800

    Change title...

--- src/php/registration_page_professor.php
+++ src/php/registration_page_professor.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Professor Registration | Research DB</title>
+    <title>Professor Registration | BSU-SC Research DB</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
