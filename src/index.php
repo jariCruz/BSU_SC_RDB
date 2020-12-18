48a4d45ade68918fc8db4240d8c79ec45a2ce3f2
jariCruz <jhari.cruz.jc@gmail.com>
2020-12-18 22:40:44 +0800

    added search function

--- src/index.php
+++ src/index.php
@@ -1,115 +1,124 @@
+<?php
+include "server.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BSU-SC Research DB</title>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BSU-SC Research DB</title>
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
-    <!-- Popper JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <!-- Popper JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
-    <!-- Font Awesome Icon -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
-    <!-- Font link -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Poppins">
-    
-    <!-- Other resources -->
-    <link rel="stylesheet" href="css/imagebg.css">
-    <link rel="stylesheet" href="css/custom.css">
-    
-  </head>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+  <!-- Font Awesome Icon -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Font link -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Poppins">
+
+  <!-- Other resources -->
+  <link rel="stylesheet" href="css/imagebg.css">
+  <link rel="stylesheet" href="css/custom.css">
+
+</head>
+
 <body>
 
 
-<!-- navigator -->
-<div class="bg" style="padding-bottom: 25%;">
-  <div class="sticky-top">
+  <!-- navigator -->
+  <div class="bg" style="padding-bottom: 25%;">
+    <div class="sticky-top">
 
 
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-      
-      <div class="container-fluid">
-        
-        <div>
-          <div class="hf">
-            <a class="navbar-brand" href="#">Research DB</a>
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+
+        <div class="container-fluid">
+
+          <div>
+            <div class="hf">
+              <a class="navbar-brand" href="#">Research DB</a>
+            </div>
+
+            <div class="mt-n3">
+              <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
+            </div>
           </div>
-      
-          <div class="mt-n3">
-            <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
-          </div>
-        </div>
 
-        <!-- Insert this below the research data bank link
+          <!-- Insert this below the research data bank link
         <span class="navbar-text">Bulacan State University - Sarmiento Campus</span>
         -->
 
-        <ul class="navbar-nav ml-auto">
+          <ul class="navbar-nav ml-auto">
 
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
 
-          </li>
+            </li>
 
 
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
 
-          </li>
+            </li>
 
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
 
-          </li>
+            </li>
 
-        </ul>
+          </ul>
 
-    
 
-        
-        
 
-      </div>
-    </nav>
-  </div>
 
-  <!-- body -->
 
-  <div class="container-fluid">
-    <div class="display-5" style="margin-top: 13%; margin-left: 8%">
+
+        </div>
+      </nav>
+    </div>
+
+    <!-- body -->
+
+    <div class="container-fluid">
+      <div class="display-5" style="margin-top: 13%; margin-left: 8%">
         <h1>Welcome to RSDB</h1>
         <small style="font-size:larger">Search the knowledge hidden within</small>
-        
+
         <!-- Search form -->
         <div class="input-group mb-3 mt-4" style="width: 50%;">
           <div class="input-group-prepend">
             <button class="btn btn-outline-primary" type="button" style="background-color: transparent;">Search</button>
           </div>
-          <input type="text" class="form-control" style="background-color: transparent;" placeholder="Search for articles...">
+          <input type="text" class="form-control" onkeyup="search(this.value)" style="background-color: transparent;" placeholder="Search for articles...">
+        </div>
+        <div id="results">
+          <!-- This is where search results be shown -->
         </div>
 
 
         <!-- Most search keywords -->
 
         <div class="container-fluid">
-          <div class="row" >
+          <div class="row">
 
             <div>
               <h6>Popular Search:</h6>
             </div>
 
-          
+
             <div class="ml-2">
               <a href="#">Fiction</a>
             </div>
@@ -131,58 +140,59 @@
 
           </div>
         </div>
+      </div>
     </div>
   </div>
-</div>
 
 
-<!-- Jumbotron -->
-<div class="jumbotron jumbotron-fluid jt">
-  <div class="container">
+  <!-- Jumbotron -->
+  <div class="jumbotron jumbotron-fluid jt">
+    <div class="container">
 
-    <h1>Lorem lorem ipsum</h1>
-    <p>Lorem ipsum dolor sit amet. Lorem ipsum lorem ipsum</p>
+      <h1>Lorem lorem ipsum</h1>
+      <p>Lorem ipsum dolor sit amet. Lorem ipsum lorem ipsum</p>
+    </div>
   </div>
-</div>
 
 
-<!-- Footer -->
+  <!-- Footer -->
 
-<footer class="border-top-2 pb-4">
+  <footer class="border-top-2 pb-4">
 
-  <div class="container">
-    <div class="row">
-      <div class="col-8 ft">
-        <p style="margin-top: -1%">Copyright © 2020 Research DB. All rights reserved.<br>
-          We use cookies to help provide and enhance our service and tailor content.<br>
-          By continuing you, agree to our <a href="#">Cookies Settings</a>.</p><br>
+    <div class="container">
+      <div class="row">
+        <div class="col-8 ft">
+          <p style="margin-top: -1%">Copyright © 2020 Research DB. All rights reserved.<br>
+            We use cookies to help provide and enhance our service and tailor content.<br>
+            By continuing you, agree to our <a href="#">Cookies Settings</a>.</p><br>
 
-        <div style="margin-top: -4%;">
-          <a href="#">Copyright</a>
-          <span class="px-3">|</span>
+          <div style="margin-top: -4%;">
+            <a href="#">Copyright</a>
+            <span class="px-3">|</span>
 
-          <a href="#">Terms of Use</a>
-          <span class="px-3">|</span>
-          
-          <a href="#">Privacy Policy</a>
-          
+            <a href="#">Terms of Use</a>
+            <span class="px-3">|</span>
+
+            <a href="#">Privacy Policy</a>
+
+          </div>
         </div>
+
+        <div class="col-4" style="margin-top: -1%">
+          <span>Follow us on:</span><br>
+
+          <span class="fa fa-facebook-official sl"></span>
+          <span class="fa fa-instagram sl px-3"></span>
+          <span class="fa fa-twitter-square sl"></span>
+        </div>
+
       </div>
-
-      <div class="col-4" style="margin-top: -1%">
-        <span>Follow us on:</span><br>
-
-        <span class="fa fa-facebook-official sl"></span>
-        <span class="fa fa-instagram sl px-3"></span>
-        <span class="fa fa-twitter-square sl"></span>
-      </div>
-
     </div>
-  </div>
 
-</footer>
-
+  </footer>
 
 
+  <script src="js/script.js"></script>
 </body>
+
 </html>
