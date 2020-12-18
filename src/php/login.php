f6d97c3a434b776d330424dd416f97e01284d79d
avidCutlet <vonwebsterlabajo@gmail.com>
2020-12-18 23:12:44 +0800

    Adding login page

---
+++ src/php/login.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in | BSU-SC Research DB</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Font link -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface|Poppins">
+
+    <!-- Other resources -->
+    <link rel="stylesheet" href="../css/custom.css">
+
+</head>
+<body>
+
+<!-- Form -->
+    <div class="container">
+        <h1 class="d-flex justify-content-center">Someone is logging in ...</h1>
+
+        <form action="../index.php" novalidate
+                class="needs-validation
+                        border border-dark rounded-lg
+                        p-5 mx-auto mb-3 reg-form-custom-width">
+
+
+            
+            
+            
+            <!-- Forename field -->
+
+            <div class="form-group">
+                <label for="form_fname">Username:</label>
+                
+                <input type="text"
+                        class="form-control"
+                        id="form_uname"
+                        placeholder="Username"
+                        name="form_uname"
+                        minlength="2"
+                        maxlength="30"
+                        required>
+
+                <!-- Notification -->
+                <div class="valid-feedback">Valid~</div>
+                <div class="invalid-feedback">Fill in this part, please~</div>
+                
+
+
+            </div>
+
+            <!-- Password field -->
+
+
+            <div class="form-group mt-2">
+                <label for="form_pass">Passsword:</label>
+                <input type="password"
+                        name="form_pass"
+                        id="form_pass"
+                        placeholder="Password"
+                        class="form-control"
+                        maxlength="30"
+                        minlength="8"
+                        required>
+
+                <!-- Notification -->
+
+                <div class="valid-feedback">Valid~</div>
+                <div class="invalid-feedback">Fill in this part, please~</div>
+
+            </div>
+
+            
+            <!-- Checkbox -->
+            <div class="form-group">
+                <input type="checkbox">
+                <span>Remember me</span>
+
+            </div>
+
+            <!-- Register btn -->
+            <div>
+                <button type="submit" class="btn btn-primary">Login</button>
+
+            </div>
+
+            <div class="d-flex justify-content-center mt-3">
+                <span>Don't have an account yet? <a href="login.php">Register here.</a></span>
+            </div>
+
+            <!-- just a freakin horizontal line -->            
+            <hr class="bg-dark">
+
+            <div class="d-flex justify-content-center">
+                <a href="#">Forgot password? Click here</a>
+            </div>
+    
+        </form>
+    </div>
+
+<!-- Disable form submissions if there are invalid fields -->
+<script>
+    
+    (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Get the forms we want to add validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+        
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+            
+            event.preventDefault();
+            event.stopPropagation();
+            }   
+            form.classList.add('was-validated');
+        }, false);
+        });
+    }, false);
+    })();
+</script>
+
+
+    
+</body>
+</html>
