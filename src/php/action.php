ffe6189ce351ff594c2c63b49027c530dbb2d98a
jariCruz <jhari.cruz.jc@gmail.com>
2020-12-19 18:58:26 +0800

    old search.php file, renamed to action.php

---
+++ src/php/action.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "user";
+$password = "password";
+$db = "BSUSC_Research_Databank";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_GET['q'])) {
+  $token = $_GET['q'];// q is from search(this.value)
+  $str = "";
+
+  $sql = " SELECT * from test_search where test_fname LIKE '%$token%' OR test_lname LIKE '%$token%' ";
+  $result = mysqli_query($conn, $sql);
+
+  if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_array($result)) {
+
+      echo '<div class="list-group">' . $row["test_fname"] . " " . $row["test_lname"] . '</div>';
+    }
+  }
+  //end if (mysqli_num_rows($result) > 0)
+  else {
+    echo "No Results Found";
+  }
+}//if(isset($_GET['q'])
