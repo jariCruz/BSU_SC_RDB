e951a8724c0e670ee919025569044db4555a0d75
jariCruz <jhari.cruz.jc@gmail.com>
2020-12-23 18:50:28 +0800

    successfully adding to database

--- src/php/rs_upload_page_function.php
+++ src/php/rs_upload_page_function.php
@@ -1,24 +1,27 @@
 <?php
   include "server.php";
-  if (isset($_POST['q'])) {
-      $rs_title = $_POST['form_title'];
-      $rs_author = $_POST['form_author'];
-      $rs_file = $_POST['form_file'];
-      $rs_year_level = $_POST['form_year'];
-      $rs_course = $_POST['form_course'];
-      $rs_advised = $_POST['form_adviser'];
-      $rs_abstract = $_POST['form_abstract'];
-      $rs_keywords = $_POST['form_keywords'];
+  $title = $conn -> real_escape_string($_POST['form_title']);
+  $abstract = $conn -> real_escape_string($_POST['form_abstract']);
+  $author = $conn -> real_escape_string($_POST['form_author']);
+  $file = $conn -> real_escape_string($_POST['form_file']);
+  $year = $conn -> real_escape_string($_POST['form_year']);
+  $course = $conn -> real_escape_string($_POST['form_course']);
+  $keywords = $conn -> real_escape_string($_POST['form_keywords']);
+  $adviser = $conn -> real_escape_string($_POST['form_adviser']);
 
-      $sql = "INSERT INTO `researchstudy_table`(`Title`, `Abstract`, `Author`, `File`, `Year`, `Course`, `Keywords`, `Adviser`)
-       VALUES ('$rs_title', '$rs_abstract', '$rs_author', '$rs_file', '$rs_year_level', '$rs_course', '$rs_keywords', '$rs_adviser')";
+  $sql = "INSERT INTO researchstudy_table (Title, Abstract, Author, File, Year, Course, Keywords, Adviser)
+  VALUES ('$title', '$abstract', '$author', '$file', '$year', '$course', '$keywords', '$adviser')";
 
-      if ($conn->query($sql) === TRUE) {
-          header("Location: rs_upload_page.php?addedsuccessfully!");
-      } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-      }
-
-      $conn->close();
+  if ($conn->query($sql) === TRUE) {
+    echo "Title: " . $title . "<br>" .
+    "Abstract: " . $abstract . "<br>" .
+    "Author: " . $author . "<br>" .
+    "File: " . $file . "<br>" .
+    "Year: " . $year . "<br>" .
+    "Course: " . $course . "<br>" .
+    "Keywords: " . $keywords . "<br>" .
+    "Adviser: " . $adviser . "<br>";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
   }
 ?>
--- src/php/server.php
+++ src/php/server.php
@@ -1,10 +1,2 @@
 <?php
-   $conn=mysqli_connect("localhost", "root", "");
-   mysqli_select_db($conn, "bsusc_research_databank");
-
-    if ($conn) {
- 	echo "";
-     }
-    else{
- 	die ("Connection failed because " .mysqli_connect_error());
- }
+  $conn = new mysqli('localhost', 'root', '', 'bsusc_research_databank');
