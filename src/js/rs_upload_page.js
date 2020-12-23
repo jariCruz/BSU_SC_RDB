a1704f2a2667c0e54a6a2e60b59e82074037557f
jariCruz <jhari.cruz.jc@gmail.com>
2020-12-23 08:59:13 +0800

    fixed upload validation

---
+++ src/js/rs_upload_page.js
@@ -0,0 +1,120 @@
+//This file contains scripts fo rs_upload_page.php
+// Individual notification
+function validateInput(n, min) {//min is for the length that is accepted
+  // Get the current element
+  var inputValue = document.getElementById(n);
+  var len = document.getElementById(min);
+  // Get the parent element
+  var pn = inputValue.parentElement;
+  // Get the value
+  var vElement = inputValue.value;
+  //validate the length
+  if (vElement.length >= len) {
+    pn.className += " was-validated";
+  }
+}
+// end of individual notification
+
+//show the current tab
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+function showTab(n) {
+  // This function will display the specified tab of the form
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ...and fix the prev/next buttons
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ...and run a function that will display the correct step indicator
+  fixStepIndicator(n);
+}
+//end of the current tab
+
+//nextPrev button function
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  if (n == 1 && !validateForm()) return true;
+  // Hide the current tab
+  x[currentTab].style.display = "none";
+  // Increase or decrease current tab by 1
+  currentTab = currentTab + n;
+  // If you have reached the end of the form
+  if (currentTab >= x.length) {
+    // the form gets submitted
+    document.getElementById("rs_upload_form").submit();
+    return false;
+  }
+  // Otherwise display the correct tab
+  showTab(currentTab);
+}
+//end of previous and next tab buttons
+
+// validate if all fields have met required values
+function validateForm() {
+  var valid = true;
+  var i = 0;
+  var divTab = document.getElementsByClassName("tab");
+  // Child nodes
+  var cnInput = divTab[currentTab].getElementsByTagName("input");//current tab input field
+  var cnSelect = divTab[currentTab].getElementsByTagName("select");//current tab select field
+  var cnTextarea = divTab[currentTab].getElementsByTagName("textarea");//current tab textarea field
+
+  // A loop that check every input field in the current tab
+  for (i = 0; i < cnInput.length; i++) {
+    // if a field is empty...
+    if (cnInput[i].value == "") {
+      // adds an invalid class to the parent node
+      cnInput[i].parentElement.className += " was-validated";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  //end loop for input field
+
+  // A loop that check every select field in the current tab
+  for (i = 0; i < cnSelect.length; i++) {
+    // if a field is empty...
+    if (cnSelect[i].selectedIndex == 0) {
+      // adds an invalid class to the parent node
+      cnSelect[i].parentElement.className += " was-validated";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  //end loop for select field
+
+  // A loop that check every textarea field in the current tab
+  for (i = 0; i < cnTextarea.length; i++) {
+    // if a field is empty...
+    if (cnTextarea[i].value == "") {
+      // adds an invalid class to the parent node
+      cnTextarea[i].parentElement.className += " was-validated";
+      // and set the current valid status to false
+      valid = false;
+    }
+  }
+  //end loop for textarea field
+
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid;
+}
+function fixStepIndicator(n) {
+  // Remove the "active" class on all steps
+  var x = document.getElementsByClassName("step");
+  for (var i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  // Add the current "active" class on the current step
+  x[n].className += " active";
+}
--- src/php/rs_upload_page.php
+++ src/php/rs_upload_page.php
@@ -29,7 +29,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha256-OFRAJNoaD8L3Br5lglV7VyLRf0itmoBzWUoM+Sji4/8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.js" integrity="sha512-VvWznBcyBJK71YKEKDMpZ0pCVxjNuKwApp4zLF3ul+CiflQi6aIJR+aZCP/qWsoFBA28avL5T5HA+RE+zrGQYg==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput-angular.min.js" integrity="sha512-KT0oYlhnDf0XQfjuCS/QIw4sjTHdkefv8rOJY5HHdNEZ6AmOh1DW/ZdSqpipe+2AEXym5D0khNu95Mtmw9VNKg==" crossorigin="anonymous"></script>
-        
+
 
 
     <!-- Other resources -->
@@ -71,7 +71,7 @@ button:hover {
   width: 15px;
   margin: 0 2px;
   background-color: #bbbbbb;
-  border: none;  
+  border: none;
   border-radius: 50%;
   display: inline-block;
   opacity: 0.5;
@@ -85,7 +85,7 @@ button:hover {
 .step.finish {
   background-color: #4CAF50;
 }
-    
+
     </style>
 
 </head>
@@ -107,19 +107,19 @@ button:hover {
                 <p class="text-danger cts-1 m-3">Note: Once the information have been uploaded, it cannot be edited again.
                     Please be sure to check the information you provided before you upload, thank you.
                 </p>
-                    
+
             </div>
 
             <!-- This tab include:
                     Title, author, year, course, and adviser
             -->
-               
+
             <div class="tab">
                 <!-- Title field -->
 
                 <div class="form-group">
                     <label for="form_title">Title:</label>
-                    
+
                     <input type="text"
                             class="form-control"
                             id="form_title"
@@ -127,13 +127,13 @@ button:hover {
                             name="form_title"
                             minlength="10"
                             maxlength="100"
-                            onkeypress="cnotif('form_title', '10')"
+                            onkeypress="validateInput('form_title', '10')"
                             required>
 
                     <!-- Notification -->
                     <div class="valid-feedback">Valid~</div>
                     <div class="invalid-feedback">Note: Minimum length 10 and maximum 100~</div>
-                    
+
 
 
                 </div>
@@ -149,7 +149,7 @@ button:hover {
                             class="form-control"
                             minlength="5"
                             maxlength="100"
-                            onkeypress="cnotif('form_author', '5')"
+                            onkeypress="validateInput('form_author', '5')"
                             required>
 
 
@@ -159,22 +159,22 @@ button:hover {
                     <div class="invalid-feedback">Note: Minimum length 5 and maximum 100~</div>
 
 
-                
+
                 </div>
 
 
                 <!-- Year level field -->
-                
+
                 <div class="row mt-3 ml-1">
                     <div class="form-group mr-1">
                             <label for="form_year">Year level:</label>
-                            
+
                             <select name="form_year"
                                     id="form_year"
                                     class="form-control
                                             select-picker
                                             border-muted"
-                                    onchange="cnotif('form_year', '')"
+                                    onchange="validateInput('form_year', '')"
                                     required>
 
                                 <option value="">Choose year level</option>
@@ -184,7 +184,7 @@ button:hover {
                                 <option value="4th year">4th year</option>
                             </select>
 
-                                    
+
 
                         <!-- Notification -->
                         <div class="valid-feedback">Valid~</div>
@@ -199,22 +199,22 @@ button:hover {
 
                             });
                         </script>
-                        
+
                     </div>
 
 
                     <!-- Course field -->
-                    
-                    
+
+
                     <div class="form-group">
                             <label for="form_course">Course:</label>
-                            
+
                             <select name="form_course"
                                     id="form_course"
                                     class="form-control
                                             select-picker
                                             border-muted"
-                                    onchange="cnotif('form_course', '')"
+                                    onchange="validateInput('form_course', '')"
                                     required>
 
                                 <option value="">Choose course</option>
@@ -222,7 +222,7 @@ button:hover {
                                 <option value="educ">EDUC</option>
                             </select>
 
-                                    
+
 
                         <!-- Notification -->
                         <div class="valid-feedback">Valid~</div>
@@ -237,7 +237,7 @@ button:hover {
 
                             });
                         </script>
-                        
+
                     </div>
 
                 </div>
@@ -256,7 +256,7 @@ button:hover {
                             class="form-control"
                             maxlength="60"
                             minlength="5"
-                            onkeypress="cnotif('form_adviser', '5')"
+                            onkeypress="validateInput('form_adviser', '5')"
                             required>
 
                     <!-- Notification -->
@@ -287,7 +287,7 @@ button:hover {
                             maxlength="60"
                             minlength="5"
                             placeholder="Keywords"
-                            onkeypress="cnotif('form_keywords', '5')"
+                            onkeypress="validateInput('form_keywords', '5')"
                             data-role="tagsinput"
                             required>
 
@@ -302,16 +302,16 @@ button:hover {
 
                 <div class="custom-file form-group">
                     <label for="form_file">File:</label>
-                    
+
                     <input type="file"
                             name="form_file"
                             id="form_file"
-                            class="custom-file-input form-control"                            
+                            class="custom-file-input form-control"
                             required>
                     <label for="form_file" class="custom-file-label cmt-1">Choose file...</label>
 
                     <!-- Script for adding the name of file to the label -->
-                    
+
                     <script>
                         $('#form_file').on('change', function(e){
                             // Get file name
@@ -319,7 +319,7 @@ button:hover {
 
                             // Replace the "Choose file..." label
                             $(this).next('.custom-file-label').html(fileName);
-                        
+
                         })
 
 
@@ -328,7 +328,7 @@ button:hover {
                     <!-- Notification -->
                     <div class="valid-feedback">Valid~</div>
                     <div class="invalid-feedback">Fill in this part, please~</div>
-                    
+
                 </div>
 
 
@@ -340,7 +340,7 @@ button:hover {
                             id="form_abstract"
                             placeholder="Abstract"
                             class="form-control min-height max-height"
-                            onkeypress="cnotif('form_abstract', '10')"
+                            onkeypress="validateInput('form_abstract', '10')"
                             rows="5"
                             minlength="10"
                             maxlength="500"
@@ -359,7 +359,7 @@ button:hover {
             <!-- Previous and next btn -->
             <div style="overflow: auto;">
                 <div style="float: right">
-                            
+
                     <button class="btn btn-secondary" type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
                     <button class="btn btn-primary" type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
                 </div>
@@ -373,175 +373,15 @@ button:hover {
                 <span class="step"></span>
 
             </div>
-            
-    
+
+
         </form>
     </div>
 
 <!-- Script for form's validation -->
 
-<script>
-    // Individual notification
-    function cnotif(n, min) {
-
-        // Get the current element
-
-        var cElement = document.getElementById(n);
-        
-        // Get the parent element
-        var pn = cElement.parentElement;
-
-        // Get the value
-        var vElement = cElement.value;
-
-        if (vElement.length >= min){
-
-            pn.className += " was-validated";
-
-        }
-
-    }
+<script src="../js/rs_upload_page.js"></script>
 
 
-    var currentTab = 0; // Current tab is set to be the first tab (0)
-    showTab(currentTab); // Display the current tab
-
-    function showTab(n) {
-        // This function will display the specified tab of the form
-        var x = document.getElementsByClassName("tab");
-
-        x[n].style.display = "block";
-        // ...and fix the prev/next buttons
-
-        if (n == 0){
-            document.getElementById("prevBtn").style.display = "none";
-        
-        } else {
-            document.getElementById("prevBtn").style.display = "inline";
-        }
-
-        if (n == (x.length - 1)){
-
-            document.getElementById("nextBtn").innerHTML = "Submit";
-        } else {
-
-            
-            document.getElementById("nextBtn").innerHTML = "Next";
-        }
-
-        // ...and run a function that will display the correct step indicator
-
-        fixStepIndicator(n);
-
-    }
-
-    function nextPrev(n){
-        // This function will figure out which tab to display
-        var x = document.getElementsByClassName("tab");
-
-        if (n == 1 && !validateForm()) return true;
-
-
-        // Hide the current tab
-        x[currentTab].style.display = "none";
-
-        // Increase or decrease current tab by 1
-        currentTab = currentTab + n;
-
-        // If you have reached the end of the form
-        if (currentTab >= x.length) {
-
-            // the form gets submitted
-            document.getElementById("rs_upload_form").submit();
-            return false;
-        }
-
-        // Otherwise display the correct tab
-        showTab(currentTab);
-
-
-    }
-
-
-    function validateForm(){
-
-        var valid = true;
-        var i = 0;
-        var divTab = document.getElementsByClassName("tab");
-
-
-        // Child nodes
-        var cnInput = divTab[currentTab].getElementsByTagName("input");
-        var cnSelect = divTab[currentTab].getElementsByTagName("select");
-        var cnTextarea = divTab[currentTab].getElementsByTagName("textarea");
-
-
-        // A loop that check every input field in the current tab
-        for (i = 0; i < cnInput.length; i++){
-
-            // if a field is empty...
-            if (cnInput[i].value == "") {
-
-                // adds an invalid class to the parent node
-                cnInput[i].parentElement.className += " was-validated";
-
-                // and set the current valid status to false
-                valid = false;        
-
-            }
-        }
-
-        // A loop that check every input field in the current tab
-        for (i = 0; i < cnSelect.length; i++){
-            
-            // if a field is empty...
-            if (cnSelect[i].selectedIndex == 0) {
-
-                // adds an invalid class to the parent node
-                cnSelect[i].parentElement.className += " was-validated";
-            
-                // and set the current valid status to false
-                valid = false;
-        
-
-            }
-        }
-
-        // Check the abstract if empty
-
-        if (cnTextarea.value == ''){
-
-            cnTextarea.parentElement.className += " was-validated";
-            valid = false;
-        }
-
-        if (valid) {
-            document.getElementsByClassName("step")[currentTab].className += " finish";
-        }
-
-        return valid;
-    }
-
-    function fixStepIndicator(n){
-        // Remove the "active" class on all steps
-
-        var x = document.getElementsByClassName("step");
-
-        for (var i = 0; i < x.length; i++){
-            x[i].className = x[i].className.replace(" active", "");
-        }
-
-        // Add the current "active" class on the current step
-
-        x[n].className += " active";
-    }
-
-
-
-
-
-</script>
-
-    
 </body>
 </html>
