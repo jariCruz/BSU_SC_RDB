4060c1a3573ffb55db7a278fb91f5fe618306328
jariCruz <jhari.cruz.jc@gmail.com>
2020-12-27 18:48:15 +0800

    finalized search

--- src/php/action.php
+++ src/php/action.php
@@ -5,13 +5,13 @@ if (isset($_GET['q'])) {
   $search = $_GET['q'];// q is from search(this.value)
   $str = "";
 
-  $sql = " SELECT * from test_search where test_fname LIKE '%$search%' OR test_lname LIKE '%$search%' ";
+  $sql = " SELECT * from researchstudy_table where Title LIKE '%$search%' OR Keywords LIKE '%$search%' ";
   $result = mysqli_query($conn, $sql);
 
   if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_array($result)) {
 
-      $str.='<a class="list-group" href="php/search_page.php">' . $row["test_fname"] . " " . $row["test_lname"] . '</a>';
+      $str.='<a class="list-group" href="php/search_page.php">'. $row["Title"] .'</a>';
     }
     echo $str;
   }
