63f1449f28d448123444b9ada927f39bff867e44
avidCutlet <vonwebsterlabajo@gmail.com>
2020-12-27 09:40:40 +0800

    account list (student&professor) finished

--- src/php/researchCoordinator_page.php
+++ src/php/researchCoordinator_page.php
@@ -97,7 +97,7 @@
          <!-- Nav tabs -->
   <ul class="nav nav-tabs">
     <li class="nav-item">
-      <a class="nav-link" href="#overview" onclick="removeActive()">Overview</a>
+      <a class="nav-link active" href="#overview" onclick="removeActive()">Overview</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#researchStudy" onclick="removeActive()">Research Study</a>
@@ -108,7 +108,7 @@
 
         <div class="dropdown-content" id="menu">
 
-            <a class="dropdown-item nav-link active"
+            <a class="dropdown-item nav-link"
                 href="#student" onclick="fixDropdown('Student')">Student</a>
 
             <a class="dropdown-item nav-link"
@@ -128,7 +128,7 @@
     <div class="tab-content">
 
         <!-- Overview tab -->
-        <div id="overview" class="container tab-pane fade"><br>
+        <div id="overview" class="container tab-pane fade active show"><br>
             <h5>Overview</h5>
             <p>This is the overview of the research coordinator's page</p>
         
@@ -142,7 +142,205 @@
         </div>
 
         <!-- Student tab -->
-        <div id="student" class="container-fluid tab-pane fade active show"><br>
+        <div id="student" class="container-fluid tab-pane fade"><br>
+            
+            <div class="row">
+
+                <!-- first column -->
+                <div class="col-sm-3">
+
+                    <p>5 Results</p>
+                    <hr>
+
+                    <!-- Account Status -->
+
+                    <label>Account Status:</label>
+                    <div class="dropdown dropright ml-3">
+                        <button class="btn btn-outline-secondary dropdown-toggle 
+                                        mw-btn-150p"
+                                id="accountStatus"
+                                data-toggle="dropdown">Select</button>
+                        
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#"
+                                onclick="changeBtnTxt('accountStatus', 'Pending')">Pending</a>
+
+                            <a class="dropdown-item" href="#"
+                                onclick="changeBtnTxt('accountStatus', 'Verified')">Verified</a>
+
+                            <a class="dropdown-item" href="#"
+                                onclick="changeBtnTxt('accountStatus', 'Denied')">Denied</a>
+
+                        </div>
+
+
+                    </div>
+
+
+                    <br>
+
+                    <!-- Sort name -->
+                    
+                    <label class="mt-5 mb-5">Sort Name:</label>
+
+
+                    <div class="dropdown dropright
+                                sf-margin-left">
+                        <button class="btn btn-outline-secondary dropdown-toggle
+                                        mw-btn-150p"
+                                data-toggle="dropdown" id="sortName">Select</button>
+
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#"
+                                onclick="changeBtnTxt('sortName', 'Ascending')">Ascending</a>
+
+                            <a class="dropdown-item" href="#"
+                                onclick="changeBtnTxt('sortName', 'Descending')">Descending</a>
+
+                        </div>
+
+                    </div>
+
+                    <br>
+
+                    <!-- Filter Department -->
+                    <label>Filter Course:</label>
+
+                    <input type="checkbox" id="bsit">
+                    <label for="#bsit">BSIT</label>
+
+                    <input type="checkbox" id="educ">
+                    <label for="#educ">EDUC</label>
+
+
+
+                <!-- first column -->
+                </div>
+
+                <!-- second column -->
+                <div class="col-sm-6">
+
+                    <p>Student Pending...</p>
+                    <hr>
+                    
+                    <!-- student account starts here -->
+                    <div class="row" id="border-bg">
+                        <div class="col-sm-8">
+
+
+                            <p>Name: MgWayre, Webster</p>
+                            <p>CYS: BSIT 4A1</p>
+                            <p>Address: Lorem ipsumn dolor sit amet.</p>
+
+
+                        </div>
+
+                        <div class="col-sm-4">
+                            <!-- Display this for pending section -->
+
+                            <button class="btn btn-danger w-btn-acc">Deny</button>
+                            <button class="btn btn-primary w-btn-acc">Verify</button>
+
+
+                            <!-- display this for verified section -->
+                            <!--
+                                <div class="alert alert-info">
+                                    <strong>Verified!</strong>
+                                </div>
+                            -->
+                            <!-- Display this for denied section -->
+                            <!--
+
+                                <div class="alert alert-danger">
+                                    <strong>Denied!</strong>
+                                </div>
+
+                            -->
+                            <!-- ********************************* -->
+                            <!--  Remove the two <br> tags below   -->
+                            <!-- when pending section isn't in use -->
+                            <!-- ********************************* -->
+
+                            <br>
+                            <br>
+
+                            <a href="#"
+                                data-target="#identification_card"
+                                data-toggle="modal">See identification card <span class="fa fa-id-card"></span></a>
+
+                            <!-- modal -->      
+                            <div class="modal fade" id="identification_card">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+
+                                        <!-- modal header -->
+                                        <div class="modal-header">
+                                                <button class="close fa fa-close" data-dismiss="modal"></button>
+                                        </div>
+
+                                        <div id="id_content" class="carousel slide">
+
+                                            <!-- indicators -->
+                                            <ul class="carousel-indicators">
+                                                <li data-target="id_content" data-slide-to="0" class="active"></li>
+                                                <li data-target="id_content" data-slide-to="1"></li>
+                                            </ul>
+
+                                            <!-- slideshow -->
+                                            <div class="carousel-inner">
+
+                                                <div class="carousel-item active">
+                                                    <img src="../img/sample.png" alt="identification card front" width="500" height="500">
+                                                </div>
+
+                                                <div class="carousel-item">
+                                                    <img src="../img/sample.png" alt="identification card back" width="500" height="500">
+                                                </div>
+
+
+                                            </div>
+
+                                            <!-- left and right controls -->
+                                            <a class="carousel-control-prev" href="#id_content" data-slide="prev">
+                                                <span class="fa fa-chevron-left" style="color: #000000"></span>
+                                            </a>
+
+                                            <a class="carousel-control-next" href="#id_content" data-slide="next">
+                                                <span class="fa fa-chevron-right" style="color: #000000"></span>
+                                            </a>
+
+                                        </div>
+
+                                        <!-- footer -->
+                                        <div class="modal-footer">
+                                            <button class="btn btn-outline-danger" data-dismiss="modal">Close</button>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    <!-- student account ends here -->
+                    </div>
+                    
+
+                <!-- second column -->
+                </div>
+
+                
+
+            <!-- row -->
+            </div>
+
+        <!-- student tab -->
+        </div>
+
+        <!-- Professor tab -->
+        <div id="professor" class="container-fluid tab-pane fade"><br>
             
             <div class="row">
 
@@ -220,7 +418,7 @@
                 <!-- second column -->
                 <div class="col-sm-6">
 
-                    <p>Pending...</p>
+                    <p>Professor Pending...</p>
                     <hr>
                     
                     <!-- student account starts here -->
@@ -243,24 +441,24 @@
 
 
                             <!-- display this for verified section -->
-<!--
+                            <!--
                                 <div class="alert alert-info">
                                     <strong>Verified!</strong>
                                 </div>
--->
+                            -->
                             <!-- Display this for denied section -->
-<!--
+                            <!--
 
                                 <div class="alert alert-danger">
                                     <strong>Denied!</strong>
                                 </div>
 
--->
+                            -->
                             <!-- ********************************* -->
                             <!--  Remove the two <br> tags below   -->
                             <!-- when pending section isn't in use -->
                             <!-- ********************************* -->
-                            
+
                             <br>
                             <br>
 
@@ -335,15 +533,8 @@
 
             <!-- row -->
             </div>
-            
-        <!-- student tab -->
-        </div>
 
         <!-- Professor tab -->
-        <div id="professor" class="container tab-pane fade"><br>
-            <h5>Professor</h5>
-            <p>This is where professor account list can be seen</p>
-
         </div>
 
 
